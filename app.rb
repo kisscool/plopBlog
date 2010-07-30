e7f9338afa1bdc8b78b4a6a1b03e28fe4a15823d
KissCool <bayle.philippe@gmail.com>
2010-07-30 15:17:10 +0200

    Big refactoring of articles edition/creation

--- app.rb
+++ app.rb
@@ -9,58 +9,93 @@ require 'sass'
 # helpers
 require File.join(File.dirname(__FILE__), 'helpers.rb')
 
+
 # entry point
 class App < Sinatra::Base
   helpers MyHelpers
+
   set :static, true
   set :public, 'public/'
 
   # loading the db model
   require File.join(File.dirname(__FILE__), 'model.rb')
 
+  # some kind of magic
   before do
     content_type :html, 'charset' => 'utf-8'
   end
 
+  ############ Indexes
+  #
   get '/' do
     haml :index
   end
 
-  get '/test' do
-    'bla bla'
-  end
-
-  # listing of all posts
+  # listing of all visible posts
   get '/posts' do
     @posts = Post.all(:visible => true)
     haml :posts
   end
 
-  # post initial creation
-  post '/posts' do
-    # we set the date
-    params[:created_at] = Time.now
-    # we set properties
-    @post = Post.new(params.reject {|k,v| k == 'tags'} )
-
-    # we extract and retrieve/tags, 
-    # while removing blanc spaces before and after them
-    if params[:tags] != "" and !params[:tags].nil? then
-      tags = params[:tags].split /[ ]*,[ ]*/
-      tags_collection = tags.collect {|t| Tag.first_or_new(:name => t)}
-      tags_collection.each {|tag| @post.tags << tag}
-    end
-    
-    @post.save
-
-    # finaly we redirect towards the article
-    redirect "/post/#{@post.short_title}"
+  # listing of all unfinished posts
+  get '/drafts' do
+    @posts = Post.all(:visible => false)
+    haml :posts
   end
+
+  ############ Edition of posts
+  #
 
   # give the form to create new post
   get '/posts/new' do
-    haml :post_new
+    #haml :post_new
+    haml :edit
   end
+
+  # get the form in order to edit an article
+  get '/post/:short_title/edit' do
+    @post = Post.first(:short_title => params[:short_title])
+    not_found if !@post
+    haml :edit
+  end
+
+
+  # create a new article
+  post '/posts' do
+    @post = Post.first_or_new(:short_title => params[:short_title])
+    if @post.saved?
+      # if the article already exists, we will not overwrite it*
+      error "An article with the same name already exists !"
+    end
+    # we set the date
+    params[:created_at] = Time.now
+
+    if @post.commit(params)
+      # finaly we redirect towards the article
+      redirect "/post/#{@post.short_title}"
+    else
+      error "Error during commit of the article in database"
+    end
+  end
+
+  # update a new article
+  # should be a 'put' for update, but seems to work only as a post method
+  post '/post/:short_title' do
+    p params
+    @post = Post.first_or_new(:short_title => params[:short_title])
+    # if we are trying to update an article, we verify first its existence
+    not_found if !@post.saved?
+    
+    if @post.commit(params)
+      # finaly we redirect towards the article
+      redirect "/post/#{@post.short_title}"
+    else
+      error "Error during commit of the article in database"
+    end
+  end
+
+  ############# Individual posts
+  #
 
   # show individual article
   get '/post/:short_title' do
@@ -69,37 +104,8 @@ class App < Sinatra::Base
     haml :post
   end
 
-  # update an article
-  # should be a 'put', but seems to work only as a post method
-  post '/post/:short_title' do
-    @post = Post.first_or_new(:short_title => params[:short_title])
-    #not_found if !@post
-
-    # we set properties
-    params_without_tags = params.reject {|k,v| k == 'tags'}
-    p params_without_tags
-    params_without_tags.each {|key,val| @post[key] = val}
-
-    # we extract and retrieve/tags, 
-    # while removing blanc spaces before and after them
-    if params[:tags] != "" and !params[:tags].nil? then
-      tags = params[:tags].split /[ ]*,[ ]*/
-      tags_collection = tags.collect {|t| Tag.first_or_new(:name => t)}
-      tags_collection.each {|tag| @post.tags << tag}
-    end
-
-    @post.save
-    
-    # finaly we redirect towards the article
-    redirect "/post/#{@post.short_title}"
-  end
-
-  # we get the form in order to edit an article
-  get '/post/:short_title/edit' do
-    @post = Post.first(:short_title => params[:short_title])
-    not_found if !@post
-    haml :edit
-  end
+  ############ Misc
+  #
 
   # this is the new shit, baby
   get '/stylesheet.css' do
--- model.rb
+++ model.rb
@@ -17,6 +17,31 @@ class Post
 
   has n, :comments
   has n, :tags, :through => Resource
+
+  ### methods
+
+  # create or update a post
+  def commit(params)
+    # we set default values for checkboxes
+    [:visible, :oneliner].each {|key| params[key] = "f" if !params[key] }
+
+    # we set properties
+    params_without_tags = params.reject {|k,v| k == 'tags'}
+    #params_without_tags.each {|key,val| attributes[key] = val}
+    self.attributes = params_without_tags
+    
+    # we extract and retrieve/tags,
+    # while removing blank spaces before and after them
+    self.tags = [] # we use a clean slate
+    if params[:tags] != "" and !params[:tags].nil? then
+      tags_raw = params[:tags].split /[ ]*,[ ]*/
+      tags_collection = tags_raw.collect {|t| Tag.first_or_new(:name => t)}
+      tags_collection.each {|tag| self.tags << tag}
+    end
+    
+    save
+  end
+
 end
 
 class Comment
